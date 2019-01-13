b3de0df0a8e8d271de6fd6f2011754fbb8e362c6
William Lima <wwlima@gmail.com>
2019-01-13 15:48:37 -0300

    Inclusão de anotações para ajustar a tabela
    Ajuste nas propriedades para gerar o banco de dados com os tamanhos
    corretos de colunas e campos requeridos.

--- richfacestestes/src/main/java/br/com/w2lima/richfacestestes/entity/Usuario.java
+++ richfacestestes/src/main/java/br/com/w2lima/richfacestestes/entity/Usuario.java
@@ -3,21 +3,34 @@ package br.com.w2lima.richfacestestes.entity;
 import java.io.Serializable;
 import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 
 @Entity
+@NamedQueries(value = {
+	    @NamedQuery(name = "Usuario.fingAll", query = "SELECT b FROM Usuario b")
+	})
 public class Usuario implements Serializable{
     private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Integer id;
+    
+    @Column(length=40, nullable=true)
     private String nome;
+    
+    @Column(length=8, nullable=false)
     private String login;
+    
+    @Column(length=6, nullable=false)
     private String senha;
+        
     private Date nascimento;
     
     public Integer getId() {
@@ -50,5 +63,11 @@ public class Usuario implements Serializable{
     public void setNascimento(Date nascimento) {
         this.nascimento = nascimento;
     }
+	@Override
+	public String toString() {
+		return "Usuario [id=" + id + ", nome=" + nome + ", login=" + login + ", senha=" + senha + ", nascimento="
+				+ nascimento + "]";
+	}
+    
     
 }
